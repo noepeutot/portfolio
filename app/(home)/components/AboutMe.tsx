0168292a49951802b926d131dfa5f9b3362d5777
noepeutot <noe.peutot@etu.univ-grenoble-alpes.fr>
2024-06-30 23:57:23 +0200

    Version 4.1 finale

--- app/(home)/components/AboutMe.tsx
+++ app/(home)/components/AboutMe.tsx
@@ -12,11 +12,11 @@ export default function AboutMe() {
                     <div className="w-full space-y-5">
                         <h1 className="text-2xl font-bold">Qui suis-je ?</h1>
                         <p>
-                            Je suis actuellement en dernière année de mon BUT Informatique à l'IUT2 de Grenoble.
-                            Ma formation m'a permis d'acquérir des compétences solide en développement logiciel, web et
-                            en gestion de base de données. Elle m'a fait découvrir le monde merveilleux du développement
-                            Web. Grâce à mes stages au CERMAV et G2Elab, j'ai pu comprendre que le développement Web est
-                            un domaine qui me passionne. C'est pour cela que j'envisage de poursuivre mes études
+                            Je suis actuellement en dernière année de mon BUT Informatique à l’IUT2 de Grenoble.
+                            Ma formation m’a permis d’acquérir des compétences solide en développement logiciel, web et
+                            en gestion de base de données. Elle m’a fait découvrir le monde merveilleux du développement
+                            Web. Grâce à mes stages au CERMAV et G2Elab, j’ai pu comprendre que le développement Web est
+                            un domaine qui me passionne. C’est pour cela que j’envisage de poursuivre mes études
                             dedans. </p>
                     </div>
                 </div>
@@ -24,11 +24,11 @@ export default function AboutMe() {
                     <div className="w-full space-y-5">
                         <h1 className="text-2xl font-bold">Mes loisirs ?</h1>
                         <p>
-                            Depuis mon plus jeune âge, j'ai touché aux jeux-vidéos. C'est une passion qui m'a permis de
-                            m'épanouir et de m'évader du monde réel. J'ai également pratiqué de nombreux sports : 4 ans
+                            Depuis mon plus jeune âge, j’ai touché aux jeux-vidéos. C’est une passion qui m’a permis de
+                            m’épanouir et de m’évader du monde réel. J’ai également pratiqué de nombreux sports : 4 ans
                             de judo, 3 ans de handball, 1 an de gymnastique, 1 an de badminton et 1 an de musculation.
-                            Le monde m'attire beaucoup et j'aspire à voyager pour découvrir de nouvelles cultures et de
-                            nouveaux paysages. J'ai déjà eu l'occasion de visiter le pays de ma mère, le Laos et je
+                            Le monde m’attire beaucoup et j’aspire à voyager pour découvrir de nouvelles cultures et de
+                            nouveaux paysages. J’ai déjà eu l’occasion de visiter le pays de ma mère, le Laos et je
                             compte y retourner dès que possible.
                         </p>
                     </div>
